e3bfc2e148b4aae5cd310c2247e7559e8878716d
ranuwahyudy <ranuwahyudy@gmail.com>
2022-03-23 15:31:47 +0700

    Update App.js and add a little style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import data from './Data';
 
-console.log(data);
-
 function App() {
   return (
     <div className="App">
       <div className='music-content'>
         <img src={data.album.images[1].url} alt='Song Cover'/>
         <h1 className='song-title'>{data.name}</h1>
-        <h2>{data.album.artists[0].name}</h2>
+        <h2 className='song-artist'>{data.album.artists[0].name}</h2>
         <button className='btn-select'>Select</button>
       </div>
     </div>
